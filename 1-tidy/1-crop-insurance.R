59078afbcb6ea2348ccfee0d6048d26de24b7b3c
Robert Dinterman <robert.dinterman@gmail.com>
2019-11-07 21:35:02 -0500

    tidy data with hacks, bad practice for including large data files and will need to remove

---
+++ 1-tidy/1-crop-insurance.R
@@ -0,0 +1,254 @@
+# Merging the RMA and NASS data for some sort of a crop insurance
+#  participation variable
+
+# ---- start --------------------------------------------------------------
+
+
+library("stringr")
+library("tidyverse")
+sumn <- function(x) ifelse(all(is.na(x)), NA, sum(x, na.rm = T))
+myspread <- function(df, key, value) {
+  # quote key
+  keyq <- rlang::enquo(key)
+  # break value vector into quotes
+  valueq <- rlang::enquo(value)
+  s <- rlang::quos(!!valueq)
+  df %>% gather(variable, value, !!!s) %>%
+    unite(temp, !!keyq, variable) %>%
+    spread(temp, value)
+}
+
+local_dir   <- "1-tidy/crop_insurance"
+if (!file.exists(local_dir)) dir.create(local_dir)
+
+rma   <- read_rds("0-data/RMA/sobscc.rds")
+plant <- read_rds("0-data/NASS/planted.rds")
+
+# There appears to be issues with fips being in multiple ASD in the early 80s
+plant <- plant %>% 
+  select(year, fips, BARLEY_impute:WHEAT) %>% 
+  group_by(year, fips) %>% 
+  summarise_all(sumn)
+
+# ---- plant-manipulate ---------------------------------------------------
+
+# Transform planted information into long form for each crop
+
+plant_long <- plant %>% 
+  ungroup() %>% 
+  gather(var, val, -year, -fips)
+
+plant_int <- plant_long %>% 
+  filter(!is.na(fips)) %>% 
+  separate(var, c("plant_com", "impute"), "_") %>% 
+  replace_na(list(impute = "planted"))
+
+# Need to remove: CANOLA, CHICKPEAS, MUSTARD, RYE,
+#  SAFFLOWER, SWEETPOTATOES, TOMATOES
+plant_int <- plant_int %>% 
+  filter(!(plant_com %in% c("CANOLA", "CHICKPEAS", "MUSTARD", "RYE",
+                            "SAFFLOWER", "SWEETPOTATOES", "TOMATOES")))
+
+
+# It turns out, LENTILS are considered DRY PEAS, so need to correct for that
+plant_peas <- plant_int %>% 
+  mutate(plant_com = ifelse(plant_com ==  "LENTILS", "PEAS", plant_com)) %>% 
+  filter(plant_com == "PEAS") %>% 
+  group_by(year, fips, plant_com, impute) %>% 
+  summarise(val = sumn(val)) %>% 
+  distinct()
+
+# Also combine sweet corn with corn
+plant_corn <- plant_int %>% 
+  mutate(plant_com = ifelse(plant_com ==  "SWEETCORN", "CORN", plant_com)) %>% 
+  filter(plant_com == "CORN") %>% 
+  group_by(year, fips, plant_com, impute) %>% 
+  summarise(val = sumn(val)) %>% 
+  distinct()
+
+plant_long <- plant_int %>% 
+  filter(!(plant_com %in% c("SWEETCORN", "CORN", "LENTILS", "PEAS"))) %>% 
+  distinct() %>% 
+  bind_rows(plant_corn, plant_peas) %>% 
+  group_by(year, fips, plant_com) %>% 
+  spread(impute, val) %>% 
+  ungroup()
+
+# LEFT OFF: I believe there is a problem with the BEANS.
+
+
+# plant_long <- plant_int %>% 
+# #  filter(plant_com != "ALL") %>% 
+#   group_by(year, fips) %>% 
+#   summarise(impute = sumn(impute),
+#             planted = sumn(planted),
+#             plant_com = "ALL") %>% 
+#   bind_rows(plant_int)
+
+
+# ---- rma-manipulate -----------------------------------------------------
+
+# Commodity and start year:
+# "BARLEY" 1924 "BEANS" 1944 "CANOLA" 1999 "CORN" 1924 "COTTON" 1928
+# "FLAXSEED" 1924 "LENTILS" 1999 "MUSTARD" 1999 "OATS" 1924 "PEANUTS" 1965
+# "PEAS" 1972 "PEPPERS" 2017 "POTATOES" 1929 "RICE" 1953 "RYE" 1931
+# "SAFFLOWER" 1999 "SORGHUM" 1940 "SOYBEANS" 1938 "SUGARBEETS" 1939
+# "SUNFLOWER" 1971 "SWEET CORN" 1977 "SWEET POTATOES" 1925 "TOMATOES" 1973
+# "WHEAT" 1919
+
+# Need to remove: CANOLA, MUSTARD, RYE, SAFFLOWER, SWEETPOTATOES, TOMATOES
+plant_keep <- c("BARLEY", "BEANS", "CORN", "COTTON", "FLAXSEED", "OATS",
+                "PEANUTS", "PEAS", "POTATOES", "RICE", "SORGHUM",
+                "SOYBEANS", "SUGARBEETS", "SUNFLOWER", "SWEETCORN", "WHEAT") #,
+# "CANOLA", "MUSTARD", "RYE", "SAFFLOWER",
+# "SWEETPOTATOES", "TOMATOES")
+
+# The crossover that makes sense (above 1990): CORN (and HYBRID CORN SEED),
+# WHEAT, SOYBEANS, OATS, GRAIN SORGHUM and SILAGE SORGHUM (as SORGHUM),
+# BARLEY, COTTON,
+# SUNFLOWERS (as SUNFLOWER), DRY BEANS (as BEANS), PEANUTS, POTATOES,
+# SUGAR BEETS (as SUGARBEETS), RICE, FLAX (as FLAXSEED),
+# GREEN PEAS AND DRY PEAS (as PEAS), SWEET CORN and FRESH MARKET SWEET CORN,
+# RYE, TOMATOES and FRESH MARKET TOMATOES,
+
+rma_plant <- rma %>% 
+  mutate(plant_com = case_when(com_code %in% c(47, 105, 46) ~ "BEANS",
+                               com_code %in% c(41, 141, 62, 341, 441) ~ "CORN",
+                               com_code %in% c(21, 22, 121, 321) ~ "COTTON",
+                               com_code == 31 ~ "FLAXSEED",
+                               com_code %in% c(75, 175) ~ "PEANUTS",
+                               com_code %in% c(67, 64) ~ "PEAS",
+                               # com_code == 67 ~ "DRYPEAS",
+                               # com_code == 64 ~ "GREENPEAS",
+                               com_code %in% c(55, 80, 18) ~ "RICE",
+                               com_code %in% c(51, 151, 50, 59) ~ "SORGHUM",
+                               com_code %in% c(181, 481, 81) ~ "SOYBEANS",
+                               com_code == 39 ~ "SUGARBEETS",
+                               com_code == 78 ~ "SUNFLOWER",
+                               com_code %in% c(44, 93, 42) ~ "CORN", #"SWEETCORN",
+                               com_code %in% c(85, 156) ~ "SWEETPOTATOES",
+                               com_code %in% c(86, 87) ~ "TOMATOES",
+                               com_code %in% c(11, 111, 311) ~ "WHEAT",
+                               T ~ com_name)) %>% 
+  filter(plant_com %in% plant_keep)
+
+# Summarise each county's information on crop insurance, notably the acreage
+#  and coverage/payouts for each crop and all of them.
+
+rma_int <- rma_plant %>% 
+  group_by(year, fips, plant_com) %>% 
+  summarise(covered = sumn(net_report_q),
+            liability = sumn(liability_amt),
+            premium = sumn(prem_amt),
+            subsidy = sumn(subsidy_amt),
+            indemnity = sumn(indem_amt)) %>% 
+  ungroup() %>% 
+  arrange(year, fips, plant_com)
+
+rma_all <- rma_plant %>% 
+  group_by(year, fips) %>% 
+  summarise(covered = sumn(net_report_q),
+            liability = sumn(liability_amt),
+            premium = sumn(prem_amt),
+            subsidy = sumn(subsidy_amt),
+            indemnity = sumn(indem_amt),
+            plant_com = "ALL") %>% 
+  ungroup()
+
+# rma_plant <- rma_int %>% 
+#   bind_rows(rma_all)
+
+# Need to only use the insurance information where we know planted acreage,
+#  turn observations without acreage into NA
+
+j5 <- rma_int %>% 
+  full_join(plant_long) %>% 
+  arrange(year, fips, plant_com) %>% 
+  mutate(surveyed = case_when(!is.na(planted) ~ 1,
+                              is.na(planted) & year <= max(plant$year) ~ 0,
+                              T ~ NA_real_),
+         survey_impute = case_when(!is.na(impute) ~ 1,
+                                   is.na(impute) & year <= max(plant$year) ~ 0,
+                                   T ~ NA_real_))
+
+# Dade county issue, change 12025 to 12086
+j5 <- mutate(j5, fips = ifelse(fips == 12025, 12086, fips))
+
+write_csv(j5, paste0(local_dir, "/crop_insurance_long.csv"))
+write_rds(j5, paste0(local_dir, "/crop_insurance_long.rds"))
+
+
+j5_all <- j5 %>% 
+  group_by(year, fips) %>% 
+  summarise(covered = sumn(covered * surveyed),
+            covered_impute = sumn(covered*survey_impute),
+            impute = sumn(impute * survey_impute),
+            planted = sumn(planted * surveyed),
+            liability = ifelse(all(is.na(planted)),
+                               NA_integer_, sumn(liability)),
+            premium = ifelse(all(is.na(planted)),
+                             NA_integer_, sumn(premium)),
+            subsidy = ifelse(all(is.na(planted)),
+                             NA_integer_, sumn(subsidy)),
+            indemnity = ifelse(all(is.na(planted)),
+                               NA_integer_, sumn(indemnity)),
+            plant_com = "ALL")
+
+# NEED to figure out how to not have survey/imputed in here
+# j6 <- j5 %>% 
+#   group_by()
+# 
+# crop_covered <- j5 %>% 
+#   select(-surveyed, -survey_impute) %>% 
+#   bind_rows(j5_all) %>% 
+#   myspread(plant_com, c(planted, impute, covered,
+#                         liability, subsidy, indemnity))
+
+write_csv(j5_all, paste0(local_dir, "/crop_insurance.csv"))
+write_rds(j5_all, paste0(local_dir, "/crop_insurance.rds"))
+
+# ---- summary-junk -------------------------------------------------------
+
+
+# Summary stats??
+
+j5_all %>% 
+  group_by(year) %>% 
+  summarise(covered = sumn(covered),
+            planted = sumn(planted)) %>% 
+  gather(var, val, -year) %>% 
+  ggplot(aes(year, val, color = var)) + geom_line()
+
+
+###
+
+j5 %>% 
+  group_by(year, plant_com) %>% 
+  summarise(covered = sumn(covered*surveyed),
+            planted = sumn(planted*surveyed)) %>% 
+  gather(var, val, -year, -plant_com) %>% 
+  ggplot(aes(year, val, color = var)) +
+  geom_line() +
+  facet_wrap(~plant_com, scales = "free") -> huh
+
+lemon::reposition_legend(huh, "bottom right", panel = "panel-4-4")
+
+# j5 %>% 
+#   group_by(year) %>% 
+#   filter(CORN_planted > 0) %>% 
+#   summarise(covered = sumn(CORN_acres),
+#             acres = sumn(CORN_planted)) %>% 
+#   gather(var, val, -year) %>% 
+#   ggplot(aes(year, val, color = var)) + geom_line()
+
+
+# spread(plant_com, -year, -fips)
+
+# WHEAT - ACRES PLANTED: lasts from 1989 until 2008 then stops. Need to manually
+#  add the spring and winter wheats together.
+# Same with SUNFLOWER - ACRES PLANTED: for non-oil type and oil type
+# RYE stops in 2009
+# Keep BEANS, DRY EDILE - ACRES PLANTED and BEANS, SNAP, PROCESSING - ACRES PLANTED
+
+# Easiest to keep the class_desc of the important ones then remove the wheat and
+#  sunflower totals
---
+++ 1-tidy/1-degree-days.R
@@ -0,0 +1,217 @@
+# Degree Day tidy data
+# Still need to 1) add additional data, 2) have a district level dataset, and
+#  3) to verify the correct FIPS are used
+
+# For ease of analysis, we are going to eliminate AK and HI
+
+# Do we need to add in variables for number of establishments? Anything else?
+
+# NOTE: are there really no observations for DC in the historical Ag Censuses?
+# Answer: really, there aren't.
+
+# ---- start --------------------------------------------------------------
+
+library("tidyverse")
+sumn <- function(x) ifelse(all(is.na(x)), NA, sum(x, na.rm = T))
+
+local_dir   <- "1-tidy/ddays"
+if (!file.exists(local_dir)) dir.create(local_dir)
+
+fipssues <- c("29510" = 29189, "51510" = 51059, "51515" = 51019,
+              "51540" = 51191, "51560" = 51005, "51570" = 51041,
+              "51580" = 51005, "51590" = 51143, "51595" = 51081,
+              "51600" = 51059, "51610" = 51059, "51620" = 51175,
+              "51630" = 51177, "51640" = 51035, "51660" = 51165,
+              "51670" = 51149, "51678" = 51163, "51683" = 51153,
+              "51685" = 51153, "51690" = 51089, "51720" = 51195,
+              "51730" = 51149, "51750" = 51121, "51770" = 51161,
+              "51775" = 51161, "51780" = 51083, "51790" = 51015,
+              "51820" = 51015, "51840" = 51069, "12025" = 12086) #,
+# "4012"  = 00000, "11001" = 00000, "51520" = 00000,
+# "51530" = 00000, "51535" = 00000, "51650" = 00000,
+# "51680" = 00000, "51700" = 00000, "51710" = 00000,
+# "51735" = 00000, "51740" = 00000, "51760" = 00000,
+# "51830" = 00000)
+
+
+# counties <- read_csv("0-data/uscourts/archived/f5a/f5a_goss.csv") %>% 
+#   rename_all(tolower) %>% 
+#   mutate(fips = if_else(fips == 12025, 12086L, fips)) %>% 
+#   filter(fips < 57000, !(fips > 15000 & fips < 16000),
+#          !(fips > 2000 & fips < 3000))
+
+counties <- read_csv("0-data/uscourts/goss_county.csv") %>% 
+  rename_all(tolower) %>% 
+  select(-name) %>% 
+  # Look up the fips with issues and replace with correct fips code
+  mutate(fips = ifelse(is.na(fipssues[as.character(fips)]),
+                       fips, fipssues[as.character(fips)])) %>% 
+  # mutate(fips = if_else(fips == 12025, 12086L, fips)) %>% 
+  filter(fips < 57000, !(fips > 15000 & fips < 16000),
+         !(fips > 2000 & fips < 3000))
+
+# Collapse Dade County:
+counties <- counties %>% 
+  group_by(year, fips) %>% 
+  summarise_all(sumn)
+
+counties <- complete(counties,
+                     fips, year = full_seq(year,1))
+
+# Problems start about here...
+# match(counties$fips, names(fipssues))
+
+santana <- read_rds("0-data/degree_days/degree_days.rds") %>% 
+  ungroup() %>% 
+  rename_all(tolower) %>% 
+  mutate(dday_good = dday10 - dday30) %>% 
+  filter(fips < 57000, !(fips > 15000 & fips < 16000),
+         !(fips > 2000 & fips < 3000))
+fdic  <- read_rds("0-data/FDIC/county/county_branches.rds") %>% 
+  ungroup() %>% 
+  mutate(fips = fips_br) %>% 
+  filter(fips < 57000, !(fips > 15000 & fips < 16000),
+         !(fips > 2000 & fips < 3000))
+unemp <- read_rds("0-data/LAU/LAU_annual.rds") %>% 
+  ungroup() %>% 
+  rename_all(tolower) %>% 
+  filter(fips < 57000, !(fips > 15000 & fips < 16000),
+         !(fips > 2000 & fips < 3000))
+districts <- read_csv("0-data/uscourts/district_counties.csv") %>% 
+  ungroup() %>% 
+  rename_all(tolower) %>% 
+  mutate(fips = parse_number(fips)) %>% 
+  filter(fips < 57000, !(fips > 15000 & fips < 16000),
+         !(fips > 2000 & fips < 3000))
+historical <- read_rds("0-data/NASS/historical_interp.rds") %>% 
+  ungroup() %>% 
+  left_join(districts) %>% 
+  filter(fips < 57000, !(fips > 15000 & fips < 16000),
+         !(fips > 2000 & fips < 3000))
+
+
+# ---- fips ---------------------------------------------------------------
+
+fips_counties   <- unique(counties$fips)
+fips_santana    <- unique(santana$fips) # obvious interpolation problems
+fips_fdic       <- unique(fdic$fips)
+fips_unemp      <- unique(unemp$fips)
+fips_historical <- unique(historical$fips)
+
+# Culprit appears to be the bankruptcy courts:
+setdiff(fips_counties, fips_santana) # fips_counties not in fips_santana
+setdiff(fips_santana, fips_counties) # fips_santana not in fips_counties
+setdiff(fips_counties, fips_fdic)
+setdiff(fips_counties, fips_unemp)
+setdiff(fips_counties, fips_historical)
+
+# Here are the bankruptcy FIPS which need to be adjusted:
+# [1]  2013  2016  2030  2040  2050  2060  2070  2080  2100  2120
+# [11]  2130  2140  2150  2160  2164  2170  2180  2185  2188  2200
+# [21]  2201  2210  2220  2230  2231  2240  2260  2261  2270  2280
+# [31]  2290  4012 11001 13150 13510 24007 24510 29193 29510 40413
+# [41] 51510 51515 51520 51530 51535 51540 51560 51570 51580 51590
+# [51] 51595 51600 51610 51620 51630 51640 51650 51660 51670 51678
+# [61] 51680 51683 51685 51690 51700 51710 51720 51730 51735 51740
+# [71] 51750 51760 51770 51775 51780 51790 51820 51830 51840
+
+
+# ---- joining ------------------------------------------------------------
+
+j5 <- full_join(santana, counties)
+
+j5 <- j5 %>% 
+  left_join(historical) %>% 
+  left_join(fdic) %>% 
+  left_join(unemp) %>% 
+  left_join(districts) %>% 
+  ungroup()
+
+j5 <- j5 %>% 
+  mutate(farms = if_else(farms < 1, 1, farms),
+         farms_interp = if_else(farms_interp < 1, 1, farms_interp),
+         farms_forward = farms) %>% 
+  group_by(fips) %>% 
+  fill(farms_forward) %>% 
+  ungroup()
+
+j5 <- mutate(j5,
+             pct_irrigated = acres_irrigated_interp / acres_interp,
+             b_rate = 10000*chap_12 / farms_forward,
+             b_rate_interp = 10000*chap_12 / farms_interp,
+             agloan_d_rate = agloans_d_alt / lnag,
+             agloan_re_d_rate = agloans_re_d_alt / lnreag,
+             agloan_all_d_rate = (agloans_d_alt + agloans_re_d_alt) /
+               (lnag + lnreag),
+             agloan_d_rate_year = agloans_d_alt_year / lnag_year,
+             agloan_re_d_rate_year = agloans_re_d_alt_year / lnreag_year,
+             agloan_all_d_rate_year = (agloans_d_alt_year +
+                                         agloans_re_d_alt_year) /
+               (lnag_year + lnreag_year),
+             livestock_share = sales_livestock_interp /
+               (sales_livestock_interp + sales_crops_interp),
+             unemp_rate = unemp / (unemp + emp))
+
+j5 <- mutate(j5,
+             pct_irrigated = case_when(pct_irrigated > 1 ~ 1,
+                                       pct_irrigated < 0 ~ 0,
+                                       is.infinite(pct_irrigated) ~ NA_real_,
+                                       T ~ pct_irrigated))
+
+
+# ---- crop-insurance -----------------------------------------------------
+
+crop_covered <- read_rds("1-tidy/crop_insurance/crop_insurance.rds")
+
+# j5unique  <- unique(j5$fips)
+# rmaunique <- unique(crop_covered$fips)
+# setdiff(j5unique, rmaunique)
+# setdiff(rmaunique, j5unique)
+
+j5 <- left_join(j5, crop_covered)
+
+
+
+# ---- production-regions -------------------------------------------------
+
+west    <- c("WASHINGTON", "OREGON", "CALIFORNIA", "NEVADA", "IDAHO", "UTAH",
+             "MONTANA", "WYOMING", "ARIZONA", "COLORADO", "NEW MEXICO")
+plains  <- c("NORTH DAKOTA", "SOUTH DAKOTA", "NEBRASKA",
+             "KANSAS", "OKLAHOMA", "TEXAS")
+midwest <- c("MINNESOTA", "IOWA", "MISSOURI", "WISCONSIN",
+             "ILLINOIS", "INDIANA", "MICHIGAN", "OHIO")
+south   <- c("ARKANSAS", "LOUISIANA", "MISSISSIPPI", "ALABAMA",
+             "GEORGIA", "FLORIDA", "SOUTH CAROLINA")
+
+j5 <- mutate(j5, production_region = case_when(state %in% west ~ "west",
+                                               state %in% plains ~ "plains",
+                                               state %in% midwest ~ "midwest",
+                                               state %in% south ~ "south",
+                                               T ~ "atlantic"))
+
+# Trying to save space, let's just keep the 1980 onward and select variables
+j6 <- j5 %>% 
+  filter(year > 1979) %>% 
+  select(year, fips, prec, dday8, dday10, dday30, dday32, dday34, dday_good,
+         total_filings:nbchap_13,
+         farms:net_cash_val,
+         farms_interp:net_cash_val_interp, total_branches:deposits,
+         lnag, lnreag, loans_d:agloans_re_d_alt,
+         agloans_d_year:indemnity, production_region, circuit)
+
+# PROBLEM: total_branches
+
+write_csv(j6, paste0(local_dir, "/ddays.csv"))
+write_rds(j6, paste0(local_dir, "/ddays.rds"))
+
+# Rod bankruptcies and crop insurance participation
+
+j5 %>% 
+  select(year, fips, farms, farms_forward, chap_12, b_rate,
+         agloan_d_rate, agloan_re_d_rate, agloan_all_d_rate,
+         covered, planted:production_region) %>% 
+  mutate(crop_part = ifelse(covered > planted, 1, covered / planted),
+         subsidy_ratio = ifelse(premium <= 0 | subsidy <= 0, 0,
+                                subsidy / (subsidy + premium)),
+         loss_ratio = ifelse(premium <= 0, 0, abs(indemnity / premium))) %>% 
+  write_csv("county_bankruptcies_insurance.csv")
