13a5b5ea2562e6e81f3b59fe22af78fa37b2abad
Robert Dinterman <robert.dinterman@gmail.com>
2020-04-06 10:31:53 -0400

    county bankruptcy script and data

--- 0-data/0-uscourts-counties.R
+++ 0-data/0-uscourts-counties.R
@@ -1,11 +1,38 @@
-# Scraping the court data for counties:
-# https://www.pacer.gov/psco/cgi-bin/county-code.pl
+# Robert Dinterman - US Court county level data
 
 # ---- start --------------------------------------------------------------
 
+library("rvest")
+library("tidyverse")
 
-library(rvest)
-library(tidyverse)
+# Create a directory for the data
+local_dir <- "0-data/uscourts"
+data_source <- paste0(local_dir, "/raw")
+if (!file.exists(local_dir)) dir.create(local_dir)
+if (!file.exists(data_source)) dir.create(data_source)
+
+git_base <- "https://raw.githubusercontent.com/rdinter/historical-bankruptcies/"
+
+git_urls <- paste0(git_base, c("master/1-tidy/bankruptcy/county_12months.csv",
+                               "master/1-tidy/bankruptcy/county_annual.csv"))
+
+git_files <- paste0(data_source, c("/county_12months.csv",
+                                   "/county_annual.csv"))
+
+download.file(git_urls, git_files)
+
+# Now grab the latest data and save them outside the raw folder
+annual <- read_csv("0-data/uscourts/raw/county_annual.csv")
+twelve <- read_csv("0-data/uscourts/raw/county_12months.csv")
+
+write_csv(annual, "0-data/uscourts/county_annual.csv")
+write_csv(twelve, "0-data/uscourts/county_twelve.csv")
+
+
+# ---- scrape -------------------------------------------------------------
+
+# Scraping the court data for counties:
+# https://www.pacer.gov/psco/cgi-bin/county-code.pl
 
 page <- "https://www.pacer.gov/psco/cgi-bin/county-code.pl"
 
@@ -40,28 +67,36 @@ scrape_courts <- function(fips, session, wait = 1){
 
 # ---- scraping -----------------------------------------------------------
 
-start <- Sys.time()
-paste0("Started at ", start)
 
-# Take the default values and cycle through them as the input across function
-fips_courts <- map(fips_list, scrape_courts, session = j5)
+if (!file.exists("0-data/uscourts/district_counties.csv")) {
+  start <- Sys.time()
+  paste0("Started at ", start)
+  
+  # Take the default values and cycle through them as the input across function
+  fips_courts <- map(fips_list, scrape_courts, session = j5)
+  
+  done <- Sys.time()
+  paste0("Finished at ", done)
+  difftime(done, start)
+  
+  fips_data <- bind_rows(fips_courts)
+  
+} else {
+  fips_data <- read_csv("0-data/uscourts/district_counties.csv")
+}
 
-done <- Sys.time()
-paste0("Finished at ", done)
-difftime(done, start)
 
-# Merge the entire list into one data.frame, then inspect it
-#  there should be 94 districts
-#  http://www.uscourts.gov/about-federal-courts/court-role-and-structure
-fips_data <- bind_rows(fips_courts)
-
-table(fips_data$CIRCUIT)
-
-table(fips_data$DISTRICT)
-length(table(fips_data$DISTRICT)) # Shows 95 districts, why?
-
-filter(fips_data, DISTRICT == "") # Problem "FIPS" are 66666, 88888, and 99999
-
+# # Merge the entire list into one data.frame, then inspect it
+# #  there should be 94 districts
+# #  http://www.uscourts.gov/about-federal-courts/court-role-and-structure
+# 
+# table(fips_data$CIRCUIT)
+# 
+# table(fips_data$DISTRICT)
+# length(table(fips_data$DISTRICT)) # Shows 95 districts, why?
+# 
+# filter(fips_data, DISTRICT == "") # Problem "FIPS" are 66666, 88888, and 99999
+# 
 
 # Export it as a .csv
 write_csv(fips_data, "0-data/uscourts/district_counties.csv")
