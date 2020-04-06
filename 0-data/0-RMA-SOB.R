04602b96de4e5a7ab165fdfe26b9e36b91de7d5a
Robert Dinterman <robert.dinterman@gmail.com>
2020-04-06 14:08:37 -0400

    new RMA site

--- 0-data/0-RMA-SOB.R
+++ 0-data/0-RMA-SOB.R
@@ -18,14 +18,15 @@ rma_base <- "https://www.rma.usda.gov/SummaryOfBusiness/StateCountyCrop"
 
 j5 <- rma_base %>% 
   read_html() %>% 
-  html_nodes(".rteContainer li~ li+ li a") %>% 
+  # html_nodes(".rteContainer li~ li+ li a") %>% 
+  html_nodes(".rteContainer li+ li a") %>% 
   html_attr("href") %>% 
   paste0("https://www.rma.usda.gov", .) %>% 
   # change up the file link as zip
   str_replace_all("ashx\\?la=en", "zip")
-# Hack, there's a duplicate of 1982 that needs to be 1983:
-j5 <- append(j5, paste0("https://www.rma.usda.gov/-/media/RMAweb/",
-                        "SCC-SOB/State-County-Crop/sobscc_1983.zip"))
+# # Hack, there's a duplicate of 1982 that needs to be 1983:
+# j5 <- append(j5, paste0("https://www.rma.usda.gov/-/media/RMAweb/",
+#                         "SCC-SOB/State-County-Crop/sobscc_1983.zip"))
 
 # ---- download -----------------------------------------------------------
 
@@ -38,133 +39,135 @@ map(j5, function(x){
   } 
 })
 
-coverage <- rma_base %>% 
-  read_html() %>% 
-  html_nodes(".col-md-12:nth-child(2) li+ li a") %>% 
-  html_attr("href") %>% 
-  paste0("https://www.rma.usda.gov", .) %>% 
-  # change up the file link as zip
-  str_replace_all("ashx\\?la=en", "zip")
-
-map(coverage, function(x){
-  file_x <- paste0(data_source, "/", basename(x))
-  if (!file.exists(file_x)) {
-    Sys.sleep(runif(1, 2, 3))
-    download.file(x, file_x)
-  }
-})
-
-type_struc <- rma_base %>% 
-  read_html() %>% 
-  html_nodes(".col-md-12~ .col-md-12+ .col-md-12 li+ li a") %>% 
-  html_attr("href") %>% 
-  paste0("https://www.rma.usda.gov", .) %>% 
-  # change up the file link as zip
-  str_replace_all("ashx\\?la=en", "zip")
-
-map(type_struc, function(x){
-  file_x <- paste0(data_source, "/", basename(x))
-  if (!file.exists(file_x)) {
-    Sys.sleep(runif(1, 2, 3))
-    download.file(x, file_x)
-  }
-})
+# coverage <- rma_base %>% 
+#   read_html() %>% 
+#   html_nodes(".col-md-12:nth-child(2) li+ li a") %>% 
+#   html_attr("href") %>% 
+#   paste0("https://www.rma.usda.gov", .) %>% 
+#   # change up the file link as zip
+#   str_replace_all("ashx\\?la=en", "zip")
+# 
+# map(coverage, function(x){
+#   file_x <- paste0(data_source, "/", basename(x))
+#   if (!file.exists(file_x)) {
+#     Sys.sleep(runif(1, 2, 3))
+#     download.file(x, file_x)
+#   }
+# })
+# 
+# type_struc <- rma_base %>% 
+#   read_html() %>% 
+#   html_nodes(".col-md-12~ .col-md-12+ .col-md-12 li+ li a") %>% 
+#   html_attr("href") %>% 
+#   paste0("https://www.rma.usda.gov", .) %>% 
+#   # change up the file link as zip
+#   str_replace_all("ashx\\?la=en", "zip")
+# 
+# map(type_struc, function(x){
+#   file_x <- paste0(data_source, "/", basename(x))
+#   if (!file.exists(file_x)) {
+#     Sys.sleep(runif(1, 2, 3))
+#     download.file(x, file_x)
+#   }
+# })
 
 # ---- read ---------------------------------------------------------------
 
-# Crop insurance Experience
-
-sobscc_files <- dir(data_source, full.names = T, pattern = "sobscc")
-
-scc_names1 <- c("year", "st_fips", "st_abrv", "cty_fips", "cty_name",
-                "com_code", "com_name", "ins_plan", "ins_plan_name", "cov_cat",
-                "pol_sold", "pol_earn_count", "pol_indem_count",
-                "unit_prem_count", "unit_indem_count", "net_report_q",
-                "liability_amt", "prem_amt", "subsidy_amt", "indem_amt",
-                "loss_ratio")
-scc_names2 <- c("year", "st_fips", "st_abrv", "cty_fips", "cty_name",
-                "com_code", "com_name", "ins_plan", "ins_plan_name", "cov_cat",
-                "delivery", "pol_sold", "pol_earn_count",
-                "pol_indem_count", "unit_prem_count", "unit_indem_count",
-                "net_report_q_type", "net_report_q", "endorse_acres",
-                "liability_amt", "prem_amt", "subsidy_amt", "indem_amt",
-                "loss_ratio")
-
-
-j5 <- map(sobscc_files, function(x){
-  temp <- read_delim(x, "|", escape_double = FALSE, col_names = FALSE,
-                     trim_ws = TRUE, col_types = cols(.default = "c"))
-  if (length(names(temp)) == 21) (names(temp) <- scc_names1) else (
-    names(temp) <- scc_names2
-  )
-  return(temp)
-})
-
-sobscc <- bind_rows(j5) %>% 
-  mutate_at(vars(year, st_fips, cty_fips, com_code, ins_plan, pol_sold,
-                 pol_earn_count, pol_indem_count, unit_prem_count,
-                 unit_indem_count, net_report_q, liability_amt, prem_amt,
-                 subsidy_amt, indem_amt, loss_ratio, endorse_acres),
-            parse_number) %>% 
-  mutate(fips = 1000*st_fips + cty_fips)
-
-write_csv(sobscc, paste0(local_dir, "/sobscc.csv"))
-write_rds(sobscc, paste0(local_dir, "/sobscc.rds"))
-
+# # Crop insurance Experience
+# sobscc_files <- dir(data_source, full.names = T, pattern = "sobscc")
+# 
+# scc_names1 <- c("year", "st_fips", "st_abrv", "cty_fips", "cty_name",
+#                 "com_code", "com_name", "ins_plan", "ins_plan_name", "cov_cat",
+#                 "pol_sold", "pol_earn_count", "pol_indem_count",
+#                 "unit_prem_count", "unit_indem_count", "net_report_q",
+#                 "liability_amt", "prem_amt", "subsidy_amt", "indem_amt",
+#                 "loss_ratio")
+# scc_names2 <- c("year", "st_fips", "st_abrv", "cty_fips", "cty_name",
+#                 "com_code", "com_name", "ins_plan", "ins_plan_name", "cov_cat",
+#                 "delivery", "pol_sold", "pol_earn_count",
+#                 "pol_indem_count", "unit_prem_count", "unit_indem_count",
+#                 "net_report_q_type", "net_report_q", "endorse_acres",
+#                 "liability_amt", "prem_amt", "subsidy_amt", "indem_amt",
+#                 "loss_ratio")
+# 
+# 
+# j5 <- map(sobscc_files, function(x){
+#   temp <- read_delim(x, "|", escape_double = FALSE, col_names = FALSE,
+#                      trim_ws = TRUE, col_types = cols(.default = "c"))
+#   if (length(names(temp)) == 21) (names(temp) <- scc_names1) else (
+#     names(temp) <- scc_names2
+#   )
+#   return(temp)
+# })
+# 
+# sobscc <- bind_rows(j5) %>% 
+#   mutate_at(vars(year, st_fips, cty_fips, com_code, ins_plan, pol_sold,
+#                  pol_earn_count, pol_indem_count, unit_prem_count,
+#                  unit_indem_count, net_report_q, liability_amt, prem_amt,
+#                  subsidy_amt, indem_amt, loss_ratio, endorse_acres),
+#             parse_number) %>% 
+#   mutate(fips = 1000*st_fips + cty_fips)
+# 
+# write_csv(sobscc, paste0(local_dir, "/sobscc.csv"))
+# write_rds(sobscc, paste0(local_dir, "/sobscc.rds"))
+# 
 # Notes: BUYUP is for all prior to 1989, then gone. In 1989, E was a category.
 #  Then in 1995, E disappeared and then A/C were the dominant forms but L did
 #  exist from 1995 to 2000
-
+# 
 # # Commodities of importance
 # sobscc %>% #filter(year == 2016) %>%
 #   group_by(year, com_name) %>%
 #   tally() %>%
 #   arrange(desc(n)) %>%
 #   print(n = 113)
-
+# 
 # What are the negative indemnity values?
-sobscc %>% 
-  filter(indem_amt < 0) %>% 
-  write.csv("double_check_RMA.csv", row.names = FALSE)
-
-# FIPS Summaries, let's do total acres
-sobscc_acres_all <- sobscc %>% 
-  filter(net_report_q_type == "Acres") %>% 
-  group_by(fips, year) %>% 
-  summarise(acres = sumn(net_report_q),
-            liability_amt = sumn(liability_amt),
-            prem_amt = sumn(prem_amt),
-            subsidy_amt = sumn(subsidy_amt),
-            indem_amt = sumn(indem_amt))
-
-write_csv(sobscc_acres_all, paste0(local_dir, "/sobscc_acres_all.csv"))
-write_rds(sobscc_acres_all, paste0(local_dir, "/sobscc_acres_all.rds"))
-
-# FIPS Summaries, let's do total acres
-sobscc_acres_com <- sobscc %>% 
-  mutate(com_name = toupper(com_name)) %>% 
-  filter(net_report_q_type == "Acres") %>% 
-  group_by(com_name, fips, year) %>% 
-  summarise(acres = sumn(net_report_q),
-            liability_amt = sumn(liability_amt),
-            prem_amt = sumn(prem_amt),
-            subsidy_amt = sumn(subsidy_amt),
-            indem_amt = sumn(indem_amt))
-
-write_csv(sobscc_acres_com, paste0(local_dir, "/sobscc_acres_com.csv"))
-write_rds(sobscc_acres_com, paste0(local_dir, "/sobscc_acres_com.rds"))
+# sobscc %>% 
+#   filter(indem_amt < 0) %>% 
+#   write.csv("double_check_RMA.csv", row.names = FALSE)
+# 
+# # FIPS Summaries, let's do total acres
+# sobscc_acres_all <- sobscc %>% 
+#   filter(net_report_q_type == "Acres") %>% 
+#   group_by(fips, year) %>% 
+#   summarise(acres = sumn(net_report_q),
+#             liability_amt = sumn(liability_amt),
+#             prem_amt = sumn(prem_amt),
+#             subsidy_amt = sumn(subsidy_amt),
+#             indem_amt = sumn(indem_amt))
+# 
+# write_csv(sobscc_acres_all, paste0(local_dir, "/sobscc_acres_all.csv"))
+# write_rds(sobscc_acres_all, paste0(local_dir, "/sobscc_acres_all.rds"))
+# 
+# # FIPS Summaries, let's do total acres
+# sobscc_acres_com <- sobscc %>% 
+#   mutate(com_name = toupper(com_name)) %>% 
+#   filter(net_report_q_type == "Acres") %>% 
+#   group_by(com_name, fips, year) %>% 
+#   summarise(acres = sumn(net_report_q),
+#             liability_amt = sumn(liability_amt),
+#             prem_amt = sumn(prem_amt),
+#             subsidy_amt = sumn(subsidy_amt),
+#             indem_amt = sumn(indem_amt))
+# 
+# write_csv(sobscc_acres_com, paste0(local_dir, "/sobscc_acres_com.csv"))
+# write_rds(sobscc_acres_com, paste0(local_dir, "/sobscc_acres_com.rds"))
 
 
 # Experience with Coverage Level
 
 sobcov_files <- dir(data_source, full.names = T, pattern = "sobcov")
+# https://www.rma.usda.gov/-/media/RMAweb/SCC-SOB/State-County-Crop-Coverage/
+#  sobsccc_1989forward-pdf.ashx?la=en
 cov_names <- c("year", "st_fips", "st_abrv", "cty_fips", "cty_name",
                "com_code", "com_name", "ins_plan", "ins_plan_name", "cov_cat",
                "delivery", "cov_lev", "pol_sold", "pol_earn_count",
                "pol_indem_count", "unit_prem_count", "unit_indem_count",
                "net_report_q_type", "net_report_q", "endorse_acres",
-               "liability_amt", "prem_amt", "subsidy_amt", "indem_amt",
-               "loss_ratio")
+               "liability_amt", "prem_amt", "subsidy_amt",
+               "private_subsidy_amt", "add_subsidy_amt", "efa_discount_amt",
+               "indem_amt", "loss_ratio")
 
 j5 <- map(sobcov_files, function(x) {
   read_delim(x, "|", escape_double = FALSE, col_names = cov_names,
@@ -172,16 +175,48 @@ j5 <- map(sobcov_files, function(x) {
 })
 
 sobcov <- bind_rows(j5) %>% 
-  mutate_at(vars(year, st_fips, cty_fips, com_code, ins_plan, cov_lev,
-                 pol_sold, pol_earn_count, pol_indem_count, unit_prem_count,
-                 unit_indem_count, net_report_q, liability_amt, prem_amt,
-                 subsidy_amt, indem_amt, loss_ratio, endorse_acres),
+  mutate_at(vars(-st_abrv, -cty_name, -com_name, -ins_plan_name, -cov_cat,
+                 -delivery, -net_report_q_type),
             parse_number) %>% 
+  # vars(year, st_fips, cty_fips, com_code, ins_plan, cov_lev,
+  #      pol_sold, pol_earn_count, pol_indem_count, unit_prem_count,
+  #      unit_indem_count, net_report_q, liability_amt, prem_amt,
+  #      subsidy_amt, indem_amt, loss_ratio, endorse_acres)
   mutate(fips = 1000*st_fips + cty_fips)
 
 write_csv(sobcov, paste0(local_dir, "/sobcov.csv"))
 write_rds(sobcov, paste0(local_dir, "/sobcov.rds"))
 
+# FIPS Summaries, let's do total acres
+sobcov_acres_all <- sobcov %>%
+  filter(net_report_q_type == "Acres") %>%
+  group_by(fips, year) %>%
+  summarise(acres = sumn(net_report_q),
+            liability_amt = sumn(liability_amt),
+            prem_amt = sumn(prem_amt),
+            subsidy_amt = sumn(subsidy_amt),
+            indem_amt = sumn(indem_amt))
+
+write_csv(sobcov_acres_all, paste0(local_dir, "/sobcov_acres_all.csv"))
+write_rds(sobcov_acres_all, paste0(local_dir, "/sobcov_acres_all.rds"))
+
+# FIPS Summaries, let's do total acres
+sobcov_acres_com <- sobcov %>%
+  mutate(com_name = toupper(com_name)) %>%
+  filter(net_report_q_type == "Acres") %>%
+  group_by(com_name, fips, year) %>%
+  summarise(acres = sumn(net_report_q),
+            liability_amt = sumn(liability_amt),
+            prem_amt = sumn(prem_amt),
+            subsidy_amt = sumn(subsidy_amt),
+            indem_amt = sumn(indem_amt))
+
+write_csv(sobcov_acres_com, paste0(local_dir, "/sobcov_acres_com.csv"))
+write_rds(sobcov_acres_com, paste0(local_dir, "/sobcov_acres_com.rds"))
+
+
+
+# ---- todo ---------------------------------------------------------------
 
 # Coverage level type practice
 
--- 0-data/0-RMA-cause.R
+++ 0-data/0-RMA-cause.R
@@ -1,6 +1,6 @@
 # RMA Cause of Loss 
 # https://legacy.rma.usda.gov/data/cause
-
+# https://www.rma.usda.gov/Information-Tools/Summary-of-Business/Cause-of-Loss
 
 # ---- start --------------------------------------------------------------
 
@@ -18,135 +18,187 @@ if (!file.exists(data_source)) dir.create(data_source, recursive = T)
 # ---- download -----------------------------------------------------------
 
 
-rma_base <- "https://legacy.rma.usda.gov/data/cause"
+rma_base <- paste0("https://www.rma.usda.gov/Information-Tools/",
+                   "Summary-of-Business/Cause-of-Loss")
 
-indem_only <- rma_base %>% 
+j5 <- rma_base %>% 
   read_html() %>% 
-  html_nodes("ul:nth-child(5) li:nth-child(4) a") %>% 
+  html_nodes("p a") %>% 
   html_attr("href") %>% 
-  paste0("https://legacy.rma.usda.gov", .)
+  paste0("https://www.rma.usda.gov", .) %>% 
+  # change up the file link as zip
+  str_replace_all("ashx\\?la=en", "zip")
 
-map(indem_only, function(x){
-  Sys.sleep(runif(1, 2, 3))
+map(j5, function(x){
   file_x <- paste0(data_source, "/", basename(x))
-  if (!file.exists(file_x)) download.file(x, file_x)
+  if (!file.exists(file_x)) {
+    Sys.sleep(runif(1, 2, 3))
+    download.file(x, file_x)
+  } 
 })
 
-indem_month <- rma_base %>% 
-  read_html() %>% 
-  html_nodes("br~ ul li:nth-child(4) a") %>% 
-  html_attr("href") %>% 
-  paste0("https://legacy.rma.usda.gov", .)
+# ---- colsom -------------------------------------------------------------
 
-map(indem_month, function(x){
-  Sys.sleep(runif(1, 2, 3))
-  file_x <- paste0(data_source, "/", basename(x))
-  if (!file.exists(file_x)) download.file(x, file_x)
-})
-
-sob_month <- rma_base %>% 
-  read_html() %>% 
-  html_nodes("ul:nth-child(11) li~ li+ li a") %>% 
-  html_attr("href") %>% 
-  paste0("https://legacy.rma.usda.gov", .)
-
-map(sob_month, function(x){
-  Sys.sleep(runif(1, 2, 3))
-  file_x <- paste0(data_source, "/", basename(x))
-  if (!file.exists(file_x)) download.file(x, file_x)
-})
-
-# ---- indemnity ----------------------------------------------------------
-
-j5 <- dir(data_source, pattern = "col_indem*", full.names = T)
-
-indem <- c("year", "st_fips", "st_abrv", "cty_fips", "cty_name",
-           "com_code", "com_name", "ins_plan", "ins_plan_name",
-           "stage_code", "cause_loss_code", "cause_loss_description",
-           "determined_acres", "indem_amt")
-
-sob_map <- map(j5, function(x) {
-  temp <- read_delim(x, delim = "|", col_names = indem,
-                     col_types = cols(.default = "c"), quote = "")
-  return(temp)
-})
-
-sob_indem <- sob_map %>% 
-  bind_rows() %>% 
-  mutate_at(vars(year, st_fips, cty_fips, ins_plan, cause_loss_code,
-                 com_code, determined_acres, indem_amt),
-            parse_number) %>% 
-  mutate_at(vars(cty_name, com_name, cause_loss_description), trimws)
-
-write_csv(sob_indem, paste0(local_dir, "/sob_indem.csv"))
-write_rds(sob_indem, paste0(local_dir, "/sob_indem.rds"))
-
-# ---- indem-mnth ----------------------------------------------------------
-
-j5 <- dir(data_source, pattern = "col_month*", full.names = T)
-
-indemmnth <- c("year", "st_fips", "st_abrv", "cty_fips", "cty_name",
-               "com_code", "com_name", "ins_plan", "ins_plan_name",
-               "stage_code", "cause_loss_code", "cause_loss_description",
-               "month_loss_num", "month_loss",
-               "determined_acres", "indem_amt")
-
-sob_map <- map(j5, function(x) {
-  temp <- read_delim(x, delim = "|", col_names = indemmnth,
-                     col_types = cols(.default = "c"), quote = "")
-  return(temp)
-})
-
-sob_indemmnth <- sob_map %>% 
-  bind_rows() %>% 
-  mutate_at(vars(year, st_fips, cty_fips, ins_plan, cause_loss_code,
-                 com_code, month_loss_num, determined_acres, indem_amt),
-            parse_number) %>% 
-  mutate_at(vars(cty_name, com_name, cause_loss_description), trimws)
-
-write_csv(sob_indemmnth, paste0(local_dir, "/sob_indemmnth.csv"))
-write_rds(sob_indemmnth, paste0(local_dir, "/sob_indemmnth.rds"))
-
-# ---- sob-cause ----------------------------------------------------------
 
 j5 <- dir(data_source, pattern = "colsom*", full.names = T)
+# https://www.rma.usda.gov/-/media/RMAweb/Cause-Of-Loss/
+# Summary-of-Business-with-Month-of-Loss/colsommonth_allyears-pdf.ashx?la=en
+cause_vars <- c("year", "st_fips", "st_abrv", "cty_fips", "cty_name",
+                "com_code", "com_name", "ins_plan", "ins_plan_name",
+                "cov_cat", "stage_code", "cause_loss_code",
+                "cause_loss_description", "month_loss_num", "month_loss",
+                "year_loss", "policies_prem", "policies_indem",
+                "net_planted_acres", "net_endorsed_acres", "liability_amt",
+                "base_prem_amt", "prod_paid_prem_amt", "subsidy_amt",
+                "private_subsidy_amt", "add_subsidy_amt", "efa_discount_amt",
+                "net_determined_acres", "indem_amt", "loss_ratio")
 
-precause <- c("year", "st_fips", "state", "cty_fips", "cty_name", "crop_code",
-              "crop_name", "ins_plan", "ins_plan_name", "cov_cat",
-              "stage_code", "cause_loss_code", "cause_loss_description",
-              "month_loss_num", "month_loss", "policies_prem",
-              "policies_indem", "net_planted_acres", "liability", "total_prem",
-              "subsidy", "indem_amt", "loss_ratio")
-cause <- c("year", "st_fips", "state", "cty_fips", "cty_name", "crop_code",
-           "crop_name", "ins_plan", "ins_plan_name", "cov_cat", "stage_code",
-           "cause_loss_code", "cause_loss_description", "month_loss_num",
-           "month_loss", "policies_prem", "policies_indem",
-           "net_planted_acres", "net_endorsed_acres", "liability",
-           "total_prem", "subsidy", "net_determined_acres", "indem_amt",
-           "loss_ratio")
-
-sob_map <- map(j5, function(x) {
-  # temp_year <- parse_number(str_sub(x, -8, -5))
-  # if (temp_year > 2000) {
-  #   temp <- read_delim(x, delim = "|", col_names = cause,
-  #                      col_types = cols(.default = "c"))
-  # } else if (temp_year < 2001) {
-  #   temp <- read_delim(x, delim = "|", col_names = precause,
-  #                      col_types = cols(.default = "c"))
-  # }
-  temp <- read_delim(x, delim = "|", col_names = cause,
-                     col_types = cols(.default = "c"), quote = "")
-  return(temp)
+colsom <- map(j5, function(x) {
+  read_delim(x, "|", escape_double = FALSE, col_names = cause_vars,
+             trim_ws = TRUE, col_types = cols(.default = "c"))
 })
 
-sob_cause <- sob_map %>% 
-  bind_rows() %>% 
-  mutate_at(vars(year, st_fips, cty_fips, crop_code, ins_plan, cause_loss_code,
-                 month_loss_num, policies_prem, policies_indem,
-                 net_planted_acres, net_endorsed_acres, liability, total_prem,
-                 subsidy, net_determined_acres, indem_amt, loss_ratio),
+colsom_all <- bind_rows(colsom) %>% 
+  mutate_at(vars(-st_abrv, -cty_name, -com_name, -ins_plan_name, -cov_cat,
+                 -stage_code, -cause_loss_code, -cause_loss_description,
+                 -month_loss),
             parse_number) %>% 
-  mutate_at(vars(cty_name, crop_name, cause_loss_description), trimws)
+  mutate(fips = 1000*st_fips + cty_fips)
 
-write_csv(sob_cause, paste0(local_dir, "/sob_cause.csv"))
-write_rds(sob_cause, paste0(local_dir, "/sob_cause.rds"))
+write_csv(colsom_all, paste0(local_dir, "/sob_colsom.csv"))
+write_rds(colsom_all, paste0(local_dir, "/sob_colsom.rds"))
+
+# # ---- legacy -------------------------------------------------------------
+# 
+# rma_base <- "https://legacy.rma.usda.gov/data/cause"
+# 
+# indem_only <- rma_base %>% 
+#   read_html() %>% 
+#   html_nodes("ul:nth-child(5) li:nth-child(4) a") %>% 
+#   html_attr("href") %>% 
+#   paste0("https://legacy.rma.usda.gov", .)
+# 
+# map(indem_only, function(x){
+#   Sys.sleep(runif(1, 2, 3))
+#   file_x <- paste0(data_source, "/", basename(x))
+#   if (!file.exists(file_x)) download.file(x, file_x)
+# })
+# 
+# indem_month <- rma_base %>% 
+#   read_html() %>% 
+#   html_nodes("br~ ul li:nth-child(4) a") %>% 
+#   html_attr("href") %>% 
+#   paste0("https://legacy.rma.usda.gov", .)
+# 
+# map(indem_month, function(x){
+#   Sys.sleep(runif(1, 2, 3))
+#   file_x <- paste0(data_source, "/", basename(x))
+#   if (!file.exists(file_x)) download.file(x, file_x)
+# })
+# 
+# sob_month <- rma_base %>% 
+#   read_html() %>% 
+#   html_nodes("ul:nth-child(11) li~ li+ li a") %>% 
+#   html_attr("href") %>% 
+#   paste0("https://legacy.rma.usda.gov", .)
+# 
+# map(sob_month, function(x){
+#   Sys.sleep(runif(1, 2, 3))
+#   file_x <- paste0(data_source, "/", basename(x))
+#   if (!file.exists(file_x)) download.file(x, file_x)
+# })
+# 
+# # ---- indemnity-lgcy -------------------------------------------------------
+# 
+# j5 <- dir(data_source, pattern = "col_indem*", full.names = T)
+# 
+# indem <- c("year", "st_fips", "st_abrv", "cty_fips", "cty_name",
+#            "com_code", "com_name", "ins_plan", "ins_plan_name",
+#            "stage_code", "cause_loss_code", "cause_loss_description",
+#            "determined_acres", "indem_amt")
+# 
+# sob_map <- map(j5, function(x) {
+#   temp <- read_delim(x, delim = "|", col_names = indem,
+#                      col_types = cols(.default = "c"), quote = "")
+#   return(temp)
+# })
+# 
+# sob_indem <- sob_map %>% 
+#   bind_rows() %>% 
+#   mutate_at(vars(year, st_fips, cty_fips, ins_plan, cause_loss_code,
+#                  com_code, determined_acres, indem_amt),
+#             parse_number) %>% 
+#   mutate_at(vars(cty_name, com_name, cause_loss_description), trimws)
+# 
+# write_csv(sob_indem, paste0(local_dir, "/sob_indem.csv"))
+# write_rds(sob_indem, paste0(local_dir, "/sob_indem.rds"))
+# 
+# # ---- indem-mnth-lgcy -----------------------------------------------------
+# 
+# j5 <- dir(data_source, pattern = "col_month*", full.names = T)
+# 
+# indemmnth <- c("year", "st_fips", "st_abrv", "cty_fips", "cty_name",
+#                "com_code", "com_name", "ins_plan", "ins_plan_name",
+#                "stage_code", "cause_loss_code", "cause_loss_description",
+#                "month_loss_num", "month_loss",
+#                "determined_acres", "indem_amt")
+# 
+# sob_map <- map(j5, function(x) {
+#   temp <- read_delim(x, delim = "|", col_names = indemmnth,
+#                      col_types = cols(.default = "c"), quote = "")
+#   return(temp)
+# })
+# 
+# sob_indemmnth <- sob_map %>% 
+#   bind_rows() %>% 
+#   mutate_at(vars(year, st_fips, cty_fips, ins_plan, cause_loss_code,
+#                  com_code, month_loss_num, determined_acres, indem_amt),
+#             parse_number) %>% 
+#   mutate_at(vars(cty_name, com_name, cause_loss_description), trimws)
+# 
+# write_csv(sob_indemmnth, paste0(local_dir, "/sob_indemmnth.csv"))
+# write_rds(sob_indemmnth, paste0(local_dir, "/sob_indemmnth.rds"))
+# 
+# # ---- sob-cause-lgcy -------------------------------------------------------
+# 
+# j5 <- dir(data_source, pattern = "colsom*", full.names = T)
+# 
+# precause <- c("year", "st_fips", "state", "cty_fips", "cty_name", "crop_code",
+#               "crop_name", "ins_plan", "ins_plan_name", "cov_cat",
+#               "stage_code", "cause_loss_code", "cause_loss_description",
+#               "month_loss_num", "month_loss", "policies_prem",
+#               "policies_indem", "net_planted_acres", "liability", "total_prem",
+#               "subsidy", "indem_amt", "loss_ratio")
+# cause <- c("year", "st_fips", "state", "cty_fips", "cty_name", "crop_code",
+#            "crop_name", "ins_plan", "ins_plan_name", "cov_cat", "stage_code",
+#            "cause_loss_code", "cause_loss_description", "month_loss_num",
+#            "month_loss", "policies_prem", "policies_indem",
+#            "net_planted_acres", "net_endorsed_acres", "liability",
+#            "total_prem", "subsidy", "net_determined_acres", "indem_amt",
+#            "loss_ratio")
+# 
+# sob_map <- map(j5, function(x) {
+#   # temp_year <- parse_number(str_sub(x, -8, -5))
+#   # if (temp_year > 2000) {
+#   #   temp <- read_delim(x, delim = "|", col_names = cause,
+#   #                      col_types = cols(.default = "c"))
+#   # } else if (temp_year < 2001) {
+#   #   temp <- read_delim(x, delim = "|", col_names = precause,
+#   #                      col_types = cols(.default = "c"))
+#   # }
+#   temp <- read_delim(x, delim = "|", col_names = cause,
+#                      col_types = cols(.default = "c"), quote = "")
+#   return(temp)
+# })
+# 
+# sob_cause <- sob_map %>% 
+#   bind_rows() %>% 
+#   mutate_at(vars(year, st_fips, cty_fips, crop_code, ins_plan, cause_loss_code,
+#                  month_loss_num, policies_prem, policies_indem,
+#                  net_planted_acres, net_endorsed_acres, liability, total_prem,
+#                  subsidy, net_determined_acres, indem_amt, loss_ratio),
+#             parse_number) %>% 
+#   mutate_at(vars(cty_name, crop_name, cause_loss_description), trimws)
+# 
+# write_csv(sob_cause, paste0(local_dir, "/sob_cause.csv"))
+# write_rds(sob_cause, paste0(local_dir, "/sob_cause.rds"))
