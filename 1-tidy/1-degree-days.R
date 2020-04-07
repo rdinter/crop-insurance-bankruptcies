f76d0ece227f8f000dda9f5400c8c129ceff2f99
Robert Dinterman <robert.dinterman@gmail.com>
2020-04-07 10:08:51 -0400

    updated data

--- 1-tidy/1-degree-days.R
+++ 1-tidy/1-degree-days.R
@@ -17,7 +17,7 @@ sumn <- function(x) ifelse(all(is.na(x)), NA, sum(x, na.rm = T))
 local_dir   <- "1-tidy/ddays"
 if (!file.exists(local_dir)) dir.create(local_dir)
 
-fipssues <- c("29510" = 29189, "51510" = 51059, "51515" = 51019,
+fipcross <- c("29510" = 29189, "51510" = 51059, "51515" = 51019,
               "51540" = 51191, "51560" = 51005, "51570" = 51041,
               "51580" = 51005, "51590" = 51143, "51595" = 51081,
               "51600" = 51059, "51610" = 51059, "51620" = 51175,
@@ -34,24 +34,28 @@ fipssues <- c("29510" = 29189, "51510" = 51059, "51515" = 51019,
 # "51830" = 00000)
 
 
+# --- bankruptcies --------------------------------------------------------
+
+
 # counties <- read_csv("0-data/uscourts/archived/f5a/f5a_goss.csv") %>% 
 #   rename_all(tolower) %>% 
 #   mutate(fips = if_else(fips == 12025, 12086L, fips)) %>% 
 #   filter(fips < 57000, !(fips > 15000 & fips < 16000),
 #          !(fips > 2000 & fips < 3000))
 
-counties <- read_csv("0-data/uscourts/goss_county.csv") %>% 
+counties <- read_csv("0-data/uscourts/county_annual.csv") %>% 
   rename_all(tolower) %>% 
-  select(-name) %>% 
   # Look up the fips with issues and replace with correct fips code
-  mutate(fips = ifelse(is.na(fipssues[as.character(fips)]),
-                       fips, fipssues[as.character(fips)])) %>% 
+  mutate(fips = ifelse(is.na(fipcross[as.character(fips)]),
+                       fips, fipcross[as.character(fips)]),
+         year = lubridate::year(date)) %>% 
   # mutate(fips = if_else(fips == 12025, 12086L, fips)) %>% 
   filter(fips < 57000, !(fips > 15000 & fips < 16000),
          !(fips > 2000 & fips < 3000))
 
 # Collapse Dade County:
 counties <- counties %>% 
+  select(-date) %>% 
   group_by(year, fips) %>% 
   summarise_all(sumn)
 
@@ -59,7 +63,17 @@ counties <- complete(counties,
                      fips, year = full_seq(year,1))
 
 # Problems start about here...
-# match(counties$fips, names(fipssues))
+# match(counties$fips, names(fipcross))
+
+districts <- read_csv("0-data/uscourts/district_counties.csv") %>% 
+  ungroup() %>% 
+  rename_all(tolower) %>% 
+  mutate(fips = parse_number(fips)) %>% 
+  filter(fips < 57000, !(fips > 15000 & fips < 16000),
+         !(fips > 2000 & fips < 3000))
+
+# ---- independent-vars ---------------------------------------------------
+
 
 santana <- read_rds("0-data/degree_days/degree_days.rds") %>% 
   ungroup() %>% 
@@ -67,22 +81,20 @@ santana <- read_rds("0-data/degree_days/degree_days.rds") %>%
   mutate(dday_good = dday10 - dday30) %>% 
   filter(fips < 57000, !(fips > 15000 & fips < 16000),
          !(fips > 2000 & fips < 3000))
+
 fdic  <- read_rds("0-data/FDIC/county/county_branches.rds") %>% 
   ungroup() %>% 
-  mutate(fips = fips_br) %>% 
+  mutate(fips = fips_br, year = lubridate::year(date)) %>% 
   filter(fips < 57000, !(fips > 15000 & fips < 16000),
          !(fips > 2000 & fips < 3000))
+
 unemp <- read_rds("0-data/LAU/LAU_annual.rds") %>% 
   ungroup() %>% 
   rename_all(tolower) %>% 
   filter(fips < 57000, !(fips > 15000 & fips < 16000),
          !(fips > 2000 & fips < 3000))
-districts <- read_csv("0-data/uscourts/district_counties.csv") %>% 
-  ungroup() %>% 
-  rename_all(tolower) %>% 
-  mutate(fips = parse_number(fips)) %>% 
-  filter(fips < 57000, !(fips > 15000 & fips < 16000),
-         !(fips > 2000 & fips < 3000))
+
+
 historical <- read_rds("0-data/NASS/historical_interp.rds") %>% 
   ungroup() %>% 
   left_join(districts) %>% 
@@ -207,11 +219,13 @@ write_rds(j6, paste0(local_dir, "/ddays.rds"))
 # Rod bankruptcies and crop insurance participation
 
 j5 %>% 
+  filter(year > 1988) %>% 
   select(year, fips, farms, farms_forward, chap_12, b_rate,
          agloan_d_rate, agloan_re_d_rate, agloan_all_d_rate,
-         covered, planted:production_region) %>% 
+         covered, planted:production_region,
+         prec, dday8, dday10, dday30, dday32, dday34, dday_good) %>% 
   mutate(crop_part = ifelse(covered > planted, 1, covered / planted),
          subsidy_ratio = ifelse(premium <= 0 | subsidy <= 0, 0,
                                 subsidy / (subsidy + premium)),
          loss_ratio = ifelse(premium <= 0, 0, abs(indemnity / premium))) %>% 
-  write_csv("county_bankruptcies_insurance.csv")
+  write_csv(paste0(local_dir, "/county_bankruptcies_insurance.csv"))
